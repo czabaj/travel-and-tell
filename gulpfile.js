2d1b25ee3cf51f9eaa45378ab244344c41a66b39
Vaclav Novotny <novotny.v@clav.cz>
2020-02-14 22:57:14 +0100

    feat(build): prepare production build

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,29 @@ const gulp = require("gulp")
 
 function css() {
   const postcss = require("gulp-postcss")
+  const purgecss = require("@fullhuman/postcss-purgecss")({
+    // Specify the paths to all of the template files in your project
+    content: ["wwwroot/index.html", "wwwroot/src/**/*.js"],
+
+    // Include any special characters you're using in this regular expression
+    // defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  })
 
   return gulp
-    .src("src/*.css")
-    .pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
-    .pipe(gulp.concat("lib/index.css"))
+    .src("index.css")
+    .pipe(
+      postcss(
+        [
+          require("tailwindcss"),
+          require("autoprefixer"),
+          process.env.NODE_ENV === "production" && purgecss,
+        ].filter(Boolean),
+      ),
+    )
+    .pipe(gulp.dest("wwwroot"))
 }
 
+exports.css = css
 exports.default = function() {
-  gulp.watch("src/*.css", css)
+  gulp.watch("index.css", css)
 }
