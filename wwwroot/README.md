7fcf40b1961cf2c6485feb90c4465a44e856e1ff
Vaclav Novotny <novotny.v@clav.cz>
2020-02-18 09:35:48 +0100

    chore: add state management

---
+++ wwwroot/README.md
@@ -0,0 +1,3 @@
+Uses [Snowpack](https://www.snowpack.dev), transpilling free development.
+
+Search for ES module enabled package on https://www.pika.dev/search
--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,13 +1,22 @@
+import "/web_modules/preact/debug.js"
 import { render } from "/web_modules/preact.js"
+import { useState } from "/web_modules/preact/hooks.js"
+import { Provider } from "/web_modules/unistore/full/preact.es.js"
 
-import { html } from "/utils/h.js"
 import Button from "/components/Button.js"
+import Layout from "/components/Layout.js"
+import store from "/store/index.js"
+import { html } from "/utils/h.js"
 
 // Create your main app component
 function SomePreactComponent() {
+  const [value, setValue] = useState(0)
+
   return html`
     <h1 className="text-4xl font-bold text-center text-blue-500">
-      Hello, World!
+      <div>Counter: ${value}</div>
+      <button onClick=${() => setValue(value + 1)}>Increment</button>
+      <button onClick=${() => setValue(value - 1)}>Decrement</button>
     </h1>
   `
 }
@@ -15,8 +24,12 @@ function SomePreactComponent() {
 // Inject your application into the an element with the id `app`.
 render(
   html`
-    <${SomePreactComponent} />
-    <${Button} to="#">Button example<//>
+    <${Provider} store=${store}>
+      <${Layout} sidebar=${"FOO"}>
+        <${SomePreactComponent} />
+        <${Button} indigo to="#">Button example<//>
+      <//>
+    <//>
   `,
   document.getElementById("app"),
 )
