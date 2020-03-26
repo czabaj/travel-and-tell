7da9c49f299d638bac05b7ef72387b839eee4803
Vaclav Novotny <novotny.v@clav.cz>
2020-03-26 22:12:55 +0100

    feat: show points on the map

--- wwwroot/components/Map.js
+++ wwwroot/components/Map.js
@@ -1,58 +1,100 @@
 import * as R from "/web_modules/ramda.js"
 import {
   connect,
+  createSelector,
   createStructuredSelector,
   html,
   useCallback,
-  useEffect,
   useRef,
+  useState,
+  useEffect,
 } from "/utils/h.js"
-import { createMap, mapContext } from "/utils/map.js"
+import { createMap } from "/utils/map.js"
 import { photosByDateSelector, setFocusedPhotoId } from "/utils/store.js"
 import MapMarker from "./MapMarker.js"
 
+const PHOTOS = "photos"
+
 const withMap = connect(
-  createStructuredSelector({ photos: photosByDateSelector }),
+  createSelector(photosByDateSelector, photos => ({
+    geoJsonData: {
+      type: "FeatureCollection",
+      features: R.map(
+        ({ id, gps }) => ({
+          type: "Feature",
+          properties: { id, icon: "music" },
+          geometry: { type: "Point", coordinates: R.reverse(gps) },
+        }),
+        photos,
+      ),
+    },
+    photos,
+  })),
   {
     clearFocusedPhoto: setFocusedPhotoId(null),
+    setFocusedPhotoIdAction: R.flip(setFocusedPhotoId),
   },
 )
 
-function Map({ clearFocusedPhoto, photos }) {
+function Map({
+  clearFocusedPhoto,
+  geoJsonData,
+  photos,
+  setFocusedPhotoIdAction,
+}) {
   const mapRef = useRef()
+  const [mapLoaded, setMapLoaded] = useState(false)
 
   const initMap = useCallback(node => {
     const map = createMap(node)
     mapRef.current = map
 
-    map.on("popupclose", clearFocusedPhoto)
     map.on("load", () => {
-      // needed otherwise map canvas dont expand
-      window.dispatchEvent(new Event("resize"))
+      // nust trigger resize otherwise map canvas don't expand
+      map.resize()
+
+      map.addSource(PHOTOS, { type: "geojson", data: geoJsonData })
+      map.addLayer({
+        id: PHOTOS,
+        type: "symbol",
+        source: PHOTOS,
+        layout: {
+          "icon-image": "{icon}-15",
+          "icon-allow-overlap": true,
+        },
+      })
+
+      map.on("click", PHOTOS, e => {
+        var coordinates = e.features[0].geometry.coordinates.slice()
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+        }
+
+        setFocusedPhotoIdAction(e.features[0].properties.id)
+      })
+      map.on("popupclose", clearFocusedPhoto)
+
+      setMapLoaded(true)
     })
   }, [])
 
-  // useEffect(() => {
-  //   if (!R.isEmpty(photos)) {
-  //     const map = mapRef.current
-  //     const polyline = L.polyline(photos.map(R.prop("gps")), {
-  //       color: "red",
-  //     }).addTo(map)
-  //     map.fitBounds(polyline.getBounds())
-  //   }
-  // }, [photos])
+  useEffect(() => {
+    if (mapLoaded) {
+      mapRef.current.getSource(PHOTOS).setData(geoJsonData)
+    }
+  }, [geoJsonData, mapLoaded])
 
   return html`
-    <${mapContext.Provider} value=${mapRef.current}>
-      <div className="absolute inset-0" ref=${initMap}>
-        ${photos.map(
-          photo =>
-            html`
-              <${MapMarker} key=${photo.id} photo=${photo} />
-            `,
-        )}
-      </div>
-    <//>
+    <div className="absolute inset-0" ref=${initMap}>
+      ${mapLoaded &&
+        html`
+          <${MapMarker} map=${mapRef.current} photos=${photos} />
+        `}
+    </div>
   `
 }
 
--- wwwroot/components/MapMarker.js
+++ wwwroot/components/MapMarker.js
@@ -1,4 +1,5 @@
 import { createPortal } from "/web_modules/preact/compat.js"
+import * as R from "/web_modules/ramda.js"
 
 import Image from "/components/Image.js"
 import {
@@ -6,54 +7,46 @@ import {
   connect,
   createSelector,
   html,
-  useContext,
   useEffect,
   useMemo,
-  useRef,
 } from "/utils/h.js"
-import { mapContext } from "/utils/map.js"
 import { focusedPhotoIdSelector } from "/utils/store.js"
 
-const photoIdSelector = (_, { photo: { id } }) => id
+const photosSelector = (_, { photos }) => photos
 
 const withMarker = connect(
   createSelector(
     focusedPhotoIdSelector,
-    photoIdSelector,
-    (focusedPhotoId, photoId) => ({ focused: focusedPhotoId === photoId }),
+    photosSelector,
+    (focusedPhotoId, photos) => ({
+      focusedPhoto:
+        focusedPhotoId && R.find(R.whereEq({ id: focusedPhotoId }), photos),
+    }),
   ),
 )
 
-function Marker({ focused, photo: { blob, gps } }) {
-  const map = useContext(mapContext)
+function Marker({ map, focusedPhoto }) {
   const content = useMemo(() => document.createElement("div"), [])
-  const popupRef = useRef()
-  const markerRef = useRef()
-  useEffect(() => {
-    popupRef.current = new mapboxgl.Popup().setHTML(content)
-    markerRef.current = new mapboxgl.Marker()
-      .setLngLat(gps)
-      .setPopup(popupRef.current)
-      .addTo(map)
-    popupRef.current.on("open")
-    return () => {
-      markerRef.current.remove()
-    }
-  }, [gps])
-  // useEffect(() => {
-  //   if (focused !== popupRef.current.isOpen()) {
-  //     markerRef.current.togglePopup()
-  //   }
-  // }, [focused])
 
-  return createPortal(
-    html`
-      <${Fragment}>
-        <${Image} blob=${blob} />
-      <//>
-    `,
-    content,
-  )
+  useEffect(() => {
+    if (focusedPhoto) {
+      new mapboxgl.Popup()
+        .setLngLat(R.reverse(focusedPhoto.gps))
+        .setDOMContent(content)
+        .addTo(map)
+    }
+  }, [focusedPhoto])
+
+  return focusedPhoto
+    ? createPortal(
+        html`
+          <${Fragment}>
+            <${Image} blob=${focusedPhoto.blob} />
+          <//>
+        `,
+        content,
+      )
+    : null
 }
 
 export default withMarker(Marker)
